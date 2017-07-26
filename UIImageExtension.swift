1db0dc96c6a066afaaf4dda311f1c1c91a560d5f
lucas093 <lucasneuf@yahoo.fr>
2017-07-26 16:38:59 +0200

    Create UIImageExtension.swift

---
+++ UIImageExtension.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+extension UIImage {
+    
+    func normalizedImage() -> UIImage? {
+        if self.imageOrientation == UIImageOrientation.up {
+            return self
+        }
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        let normalizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return normalizedImage
+    }
+    
+    func cropToBounds(width: Double, height: Double) -> UIImage? {
+        
+        let contextImage: UIImage = UIImage(cgImage: self.cgImage!)
+        
+        let contextSize: CGSize = contextImage.size
+        
+        var posX: CGFloat = 0.0
+        var posY: CGFloat = 0.0
+        var cgwidth: CGFloat = CGFloat(width)
+        var cgheight: CGFloat = CGFloat(height)
+        
+        // See what size is longer and create the center off of that
+        if contextSize.width > contextSize.height {
+            posX = ((contextSize.width - contextSize.height) / 2)
+            posY = 0
+            cgwidth = contextSize.height
+            cgheight = contextSize.height
+        } else {
+            posX = 0
+            posY = ((contextSize.height - contextSize.width) / 2)
+            cgwidth = contextSize.width
+            cgheight = contextSize.width
+        }
+        
+        let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
+        
+        guard let cgImage = contextImage.cgImage else {
+            return nil
+        }
+        
+        // Create bitmap image from context using the rect
+        let imageRef: CGImage = cgImage.cropping(to: rect)!
+        
+        // Create a new image based on the imageRef and rotate back to the original orientation
+        let image: UIImage = UIImage(cgImage: imageRef, scale: self.scale, orientation: self.imageOrientation)
+        
+        return image
+    }
+}
+
